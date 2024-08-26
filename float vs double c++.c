ce877e6a84ce769a380ef0cbcc715347d6e9f41b
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:12:02 +0530

    Create float vs double c++.c

---
+++ float vs double c++.c
@@ -0,0 +1,11 @@
+# include <iostream>
+
+using namespace std;
+
+int main (){
+    float f1 = 35e3;
+    double d1 = 12e4;
+    cout << "float"<< f1 <<"\n";
+    cout << " double " << d1;
+    return 0;
+}
